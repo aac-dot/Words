a5e63099fe0fc3605d225724e61b6eb4845272f7
aac <ayrton.andrade1@github.com>
2021-12-15 07:53:49 -0300

    - Criação dos arquivos: activity_detail.xml, DetailActivity.kt e item_view.xml. - Adição da recyclerView em nos arquivos activity_main.xml, activity_detail.xml. - Adição do recurso de vinculação no build.gradle. - Vinculação das views em MainActivity.kt e DetailActivity.kt. - Definição do layout e do adapter das recyclerView de MainActivity.kt e DetailActivity.kt - Criação do Adapter WordAdapter.kt. - Criação do recurso (resource) arrays.xml.

--- app/src/main/java/com/android/developer/words/DetailActivity.kt
+++ app/src/main/java/com/android/developer/words/DetailActivity.kt
@@ -1,14 +1,12 @@
 package com.android.developer.words
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.developer.words.databinding.ActivityDetailBinding
-import com.android.developer.words.databinding.ActivityMainBinding
 
-class DetailActivity : AppCompatActivity {
+class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/android/developer/words/WordAdapter.kt
@@ -0,0 +1,77 @@
+package com.android.developer.words
+
+import android.content.Context
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.accessibility.AccessibilityNodeInfo
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Adapter para [RecyclerView] em [DetailActivity]
+ * */
+class WordAdapter(private val letterID : String, context : Context) : RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
+
+    private val filteredWords : List<String>
+
+    init {
+        // Recupera a lista de palavras do res/values/arrays.xml
+        val words = context.resources.getStringArray(R.array.words).toList()
+
+        // Retorna os items da coleção se a condição é true,
+        // nesse caso se um item começar com uma determinada letra, ignorando letras MAIUSCULA OU minuscula.
+        filteredWords = words.filter { it.startsWith(letterID, ignoreCase = true) }
+            // Embalha a lista de palavras recuperadas.
+            .shuffled()
+            // Retorna os primeiros n items com uma lista, nesse caso os 5 primeiros.
+            .take(5)
+            // Retorna um versão ordenada da 5 cinco primeiras letras.
+            .sorted()
+    }
+
+    class WordViewHolder(val view : View) : RecyclerView.ViewHolder(view) {
+        val button = view.findViewById<Button>(R.id.button_item)
+    }
+
+    // Cria novas view com o layout item_view como modelo.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        val layout = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+
+        layout.accessibilityDelegate = Accessibility
+
+        return WordViewHolder(layout)
+    }
+
+    // Substitui o contento de uma view existente com novos dados.
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val item = filteredWords[position]
+        val context = holder.view.context
+
+        holder.button.text = item
+    }
+
+    override fun getItemCount(): Int {
+        return filteredWords.size
+    }
+
+    companion object Accessibility : View.AccessibilityDelegate() {
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun onInitializeAccessibilityNodeInfo(host: View?, info: AccessibilityNodeInfo?) {
+            super.onInitializeAccessibilityNodeInfo(host, info)
+
+            val customString = host?.context?.getString(R.string.look_up_word)
+            val customClick =
+                AccessibilityNodeInfo.AccessibilityAction(
+                    AccessibilityNodeInfo.ACTION_CLICK,
+                    customString
+                )
+            info?.addAction(customClick)
+        }
+    }
+
+
+
+}
