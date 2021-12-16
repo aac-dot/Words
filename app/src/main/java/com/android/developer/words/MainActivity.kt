dce2ac484ffcc0ee201a356ccc7fa305a3c7a21a
aac <ayrton.andrade1@github.com>
2021-12-16 08:16:34 -0300

    - Acréscimo dos métodos: chooseLayout, setIcon, onCreateOptionsMenu e onOptionsItemSelected.

--- app/src/main/java/com/android/developer/words/MainActivity.kt
+++ app/src/main/java/com/android/developer/words/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.android.developer.words
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.developer.words.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView : RecyclerView
+    private var isLinearLayoutManager = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,11 +21,54 @@ class MainActivity : AppCompatActivity() {
 
         // Vincula o recyclerView do layout com o recyclerView da activity
         recyclerView = binding.recyclerView
+        chooseLayout()
+    }
 
-        // Define o LinearLayout para a recyclerView
-        recyclerView.layoutManager = LinearLayoutManager(this)
+    private fun chooseLayout() {
+        // Define o tipo de gerenciador de layout para a recyclerView
+        if (isLinearLayoutManager) {
+            recyclerView.layoutManager = LinearLayoutManager(this)
+        } else {
+            recyclerView.layoutManager = GridLayoutManager(this, 4)
+        }
 
-        // Define qual será o adapter da recyclerView
+        // Define o adapter LetterAdapter para recyclerView
         recyclerView.adapter = LetterAdapter()
+    }
+
+    private fun setIcon(menuItem : MenuItem?) {
+        if (menuItem == null) {
+            return
+        }
+        menuItem.icon = if (isLinearLayoutManager)
+            ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+        else ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.layout_menu, menu)
+
+        val layoutButton = menu?.findItem(R.id.action_switch_menu)
+
+        setIcon(layoutButton)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_switch_menu -> {
+
+                isLinearLayoutManager = !isLinearLayoutManager
+
+                chooseLayout()
+                setIcon(item)
+
+                return true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 }
